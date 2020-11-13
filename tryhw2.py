cc70ba70a13cea862d46a7bed76b1552687f71bb
root <root@DESKTOP-QFQEMPI.localdomain>
2020-11-12 16:07:33 -0800

    1112

--- tryhw2.py
+++ tryhw2.py
@@ -1,3 +1,4 @@
+from os.path import normcase
 from uwnet import *
 def conv_net():
     l = [   make_convolutional_layer(32, 32, 3, 8, 3, 2),
@@ -12,6 +13,22 @@ def conv_net():
             make_activation_layer(SOFTMAX)]
     return make_net(l)
 
+def conv_net_batch():
+    l = [   make_convolutional_layer(32, 32, 3, 8, 3, 2),
+            make_batchnorm_layer(8),
+            make_activation_layer(RELU),
+            make_maxpool_layer(16, 16, 8, 3, 2),
+            make_convolutional_layer(8, 8, 8, 16, 3, 1),
+            make_batchnorm_layer(16),
+            make_activation_layer(RELU),
+            make_maxpool_layer(8, 8, 16, 3, 2),
+            make_convolutional_layer(4, 4, 16, 32, 3, 1),
+            make_batchnorm_layer(32),
+            make_activation_layer(RELU),
+            make_connected_layer(512, 10),
+            make_activation_layer(SOFTMAX)]
+    return make_net(l)
+
 
 print("loading data...")
 train = load_image_classification_data("cifar/cifar.train", "cifar/cifar.labels")
@@ -21,12 +38,13 @@ print
 
 print("making model...")
 batch = 128
-iters = 500
-rate = .01
+iters = 5000
+rate = .06
 momentum = .9
 decay = .005
 
-m = conv_net()
+# m = conv_net()
+m = conv_net_batch()
 print("training...")
 train_image_classifier(m, train, batch, iters, rate, momentum, decay)
 print("done")
@@ -39,5 +57,11 @@ print("test accuracy:     %f", accuracy_net(m, test))
 # How accurate is the fully connected network vs the convnet when they use similar number of operations?
 # Why are you seeing these results? Speculate based on the information you've gathered and what you know about DL and ML.
 # Your answer:
-#
 
+# The original model gives an training accuracy of 0.4065600037574768 and a test accuracy of 0.4025000035762787. 
+# When added batch norm after eac layer, the training accuracy is 0.5467399954795837, the test accuracy is 0.5388000011444092.
+# The batch norm layers obviously improved the performance.
+
+# By trying different learning rate, the best learning rate is lr = 0.06.
+# After 500 iters, the training accuracy is 0.5551400184631348 and the test accuracy is 0.5462999939918518.
+# After 5000 iters, the training accuracy is 0.6714000105857849 and the test accuracy is 0.6220999956130981.
